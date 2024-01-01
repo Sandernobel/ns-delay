396950e92185eb9716dac2652d2b7d24859af4ad
Sander Nobel <sandernobel@Sanders-MacBook-Pro.local>
2024-01-01 21:54:11 +0100

    added connection to trips api

--- src/ns_train_data/NSConnector.py
+++ src/ns_train_data/NSConnector.py
@@ -1,37 +1,7 @@
 import os
 import requests
 import json
-
-
-class NSConnector:
-
-    def __init__(self):
-        self._credential = os.environ.get("REISINFORMATIE_API")
-        self._headers = {"Ocp-Apim-Subscription-Key": self._credential}
-        self.stations = []
-
-    def _retrieve_stations(self, station: str) -> None:
-        """Get UIC code for particular station from NS API.
-
-        :param station: full name of station to retrieve UIC code from.
-        """
-        api = f"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q={station}"
-        response = requests.get(api, headers=self._headers)
-
-        station = json.loads(response.text)
-        payload = station.get("payload")
-
-        # Station query should return only one result
-        assert len(payload) == 1, "Station name is too ambiguous."
-
-        self.stations.append(Station(payload))
-
-    def get_uic_codes(self) -> dict[str: int]:
-        """Returns UIC codes for each registered station
-
-        :return: Dictionary with name and according UIC codes.
-        """
-        uic_dict = {station.get_name(): station.get_uic_code() for station in self.stations}
+import datetime
 
 
 class Station:
@@ -46,3 +16,51 @@ class Station:
     def get_uic_code(self) -> int:
         """Get station UIC code."""
         return self.payload.get("UICCode")
+
+
+class NSConnector:
+
+    def __init__(self, departure: str, arrival: str):
+        self._credential = os.environ.get("REISINFORMATIE_API")
+        self._headers = {"Ocp-Apim-Subscription-Key": self._credential}
+        self.departure = self._add_station(departure)
+        self.arrival = self._add_station(arrival)
+
+    def _add_station(self, station: str) -> Station:
+        """Get UIC code for particular station from NS API.
+
+        :param station: full name of station to retrieve UIC code from.
+        """
+        api = f"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q={station}"
+        station = self._get_api_response(api)
+        payload = station.get("payload")
+
+        # Station query should return only one result
+        assert len(payload) == 1, "Station name is too ambiguous."
+
+        return Station(payload[0])
+
+    def _get_api_response(self, api: str) -> dict:
+        """Get NS Api response."""
+        response = requests.get(api, headers=self._headers)
+        return json.loads(response.text)
+
+    def get_uic_code(self, station: str):
+        """Get UIC code of arrival/departure station."""
+        assert station in {"arrival", "departure"}, "Station must be 'arrival' or 'departure'."
+        station = self.__getattribute__(station)
+        return station.get_uic_code()
+
+    def get_journey(self):
+        """Get Journey details."""
+        print(datetime.datetime.now())
+
+        api = f"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips" \
+              f"?originUicCode={self.get_uic_code('departure')}&destinationUicCode={self.get_uic_code('arrival')}" \
+              f"&originWalk=false&originBike=false&originCar=false&destinationWalk=false&destinationBike=false" \
+              f"&destinationCar=false&shorterChange=false&travelAssistance=false&searchForAccessibleTrip=false" \
+              f"&localTrainsOnly=false&excludeHighSpeedTrains=false&excludeTrainsWithReservationRequired=false" \
+              f"&product=GEEN&discount=NO_DISCOUNT&travelClass=2&passing=false&travelRequestType=DEFAULT"
+        journey = self._get_api_response(api)
+
+        return journey
--- src/ns_train_data/get_train_data.py
+++ src/ns_train_data/get_train_data.py
@@ -1,2 +1,6 @@
-from NSConnector import NSConnector, Station
+import datetime
 
+from NSConnector import NSConnector
+
+connector = NSConnector(departure="Amsterdam Amstel", arrival="Utrecht Centraal")
+print(connector.get_journey())
